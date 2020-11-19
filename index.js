98d81ca5f47491be2862a43917eed05ccf8bf09b
sprajjwal <sprajjwal@ucdavis.edu>
2020-11-18 17:12:48 -0800

    done with stretch challenge 1

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    io.emit('chat message', 'User connected')
+    socket.on('disconnect', () => {
+        io.emit('chat message', 'User disconnected')
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
+})
+
+http.listen(3000, ()=> {
+    console.log('listening on port 3000')
+})
